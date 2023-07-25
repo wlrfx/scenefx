1743eb615902d1408a214c3e8c2b417531ddb355
Erik Reider <35975961+ErikReider@users.noreply.github.com>
2023-07-25 10:35:28 +0200

    Removed unused decoration_data fields

--- include/types/decoration_data.h
+++ include/types/decoration_data.h
@@ -10,20 +10,7 @@ static float default_dim_color[] = {0.0f, 0.0f, 0.0f, 1.0f};
 // TODO: Rename to something like `wlr_scene_node_decoration_data`?
 struct decoration_data {
 	float alpha;
-	float saturation;
 	int corner_radius;
-	// TODO: Change name from dim to something like "overlay" (maybe replace
-	// with it's own struct?)
-	float dim;
-	float *dim_color;
-	// TODO: Change to `enum corner_location` instead?
-	bool has_titlebar;
-	// TODO: Remove blur bool and add multiple fields (maybe even it's own
-	// struct?)
-	bool blur;
-	// TODO: Remove shadow bool and add multiple fields (maybe even it's own
-	// struct?)
-	bool shadow;
 };
 
 struct decoration_data decoration_data_get_undecorated(void);
--- types/decoration_data.c
+++ types/decoration_data.c
@@ -7,12 +7,6 @@
 struct decoration_data decoration_data_get_undecorated(void) {
 	return (struct decoration_data) {
 		.alpha = 1.0f,
-		.dim = 0.0f,
-		.dim_color = default_dim_color,
-		.corner_radius = 0,
-		.saturation = 1.0f,
-		.has_titlebar = false,
-		.blur = false,
-		.shadow = false,
+		.corner_radius = 0
 	};
 }

cff3dc6d369b38567892e11c39c3ab70382f396a
William McKinnon <contact@willmckinnon.com>
2024-03-27 01:38:59 -0400

    made fx_renderer public

--- include/render/fx_renderer/fx_renderer.h
+++ include/render/fx_renderer/fx_renderer.h
@@ -2,7 +2,7 @@
 #define _FX_OPENGL_H
 
 #include <GLES2/gl2.h>
-#include <GLES2/gl2ext.h>
+#include <scenefx/render/fx_renderer/fx_renderer.h>
 #include <stdbool.h>
 #include <time.h>
 #include <wlr/render/egl.h>
@@ -10,8 +10,6 @@
 #include <wlr/render/wlr_texture.h>
 #include <wlr/util/addon.h>
 #include <wlr/util/box.h>
-
-#include "render/fx_renderer/shaders.h"
 
 struct fx_pixel_format {
 	uint32_t drm_format;
@@ -88,72 +86,6 @@ struct fx_texture *fx_get_texture(struct wlr_texture *wlr_texture);
 void fx_texture_destroy(struct fx_texture *texture);
 
 bool wlr_texture_is_fx(struct wlr_texture *wlr_texture);
-
-
-///
-/// fx_renderer
-///
-
-struct fx_renderer {
-	struct wlr_renderer wlr_renderer;
-
-	float projection[9];
-	struct wlr_egl *egl;
-	int drm_fd;
-
-	const char *exts_str;
-	struct {
-		bool EXT_read_format_bgra;
-		bool KHR_debug;
-		bool OES_egl_image_external;
-		bool OES_egl_image;
-		bool EXT_texture_type_2_10_10_10_REV;
-		bool OES_texture_half_float_linear;
-		bool EXT_texture_norm16;
-		bool EXT_disjoint_timer_query;
-	} exts;
-
-	struct {
-		PFNGLEGLIMAGETARGETTEXTURE2DOESPROC glEGLImageTargetTexture2DOES;
-		PFNGLDEBUGMESSAGECALLBACKKHRPROC glDebugMessageCallbackKHR;
-		PFNGLDEBUGMESSAGECONTROLKHRPROC glDebugMessageControlKHR;
-		PFNGLPOPDEBUGGROUPKHRPROC glPopDebugGroupKHR;
-		PFNGLPUSHDEBUGGROUPKHRPROC glPushDebugGroupKHR;
-		PFNGLEGLIMAGETARGETRENDERBUFFERSTORAGEOESPROC glEGLImageTargetRenderbufferStorageOES;
-		PFNGLGETGRAPHICSRESETSTATUSKHRPROC glGetGraphicsResetStatusKHR;
-		PFNGLGENQUERIESEXTPROC glGenQueriesEXT;
-		PFNGLDELETEQUERIESEXTPROC glDeleteQueriesEXT;
-		PFNGLQUERYCOUNTEREXTPROC glQueryCounterEXT;
-		PFNGLGETQUERYOBJECTIVEXTPROC glGetQueryObjectivEXT;
-		PFNGLGETQUERYOBJECTUI64VEXTPROC glGetQueryObjectui64vEXT;
-		PFNGLGETINTEGER64VEXTPROC glGetInteger64vEXT;
-	} procs;
-
-	struct {
-		struct quad_shader quad;
-		struct tex_shader tex_rgba;
-		struct tex_shader tex_rgbx;
-		struct tex_shader tex_ext;
-		struct box_shadow_shader box_shadow;
-		struct stencil_mask_shader stencil_mask;
-		struct blur_shader blur1;
-		struct blur_shader blur2;
-		struct blur_effects_shader blur_effects;
-	} shaders;
-
-	struct wl_list buffers; // fx_framebuffer.link
-	struct wl_list textures; // fx_texture.link
-
-	struct fx_framebuffer *current_buffer;
-	uint32_t viewport_width, viewport_height;
-
-	// Set to true when 'wlr_renderer_begin_buffer_pass' is called instead of
-	// our custom 'fx_renderer_begin_buffer_pass' function
-	bool basic_renderer;
-
-	// The region where there's blur
-	pixman_region32_t blur_padding_region;
-};
 
 bool wlr_renderer_is_fx(struct wlr_renderer *wlr_renderer);
 
